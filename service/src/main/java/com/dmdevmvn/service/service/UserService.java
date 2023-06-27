1a8b856c096f9605373144410d4af4d62a66caf5
DimaNever <dimanever86@gmail.com>
2023-06-27 12:34:51 +0600

    27.06

--- service/src/main/java/com/dmdevmvn/service/service/UserService.java
+++ service/src/main/java/com/dmdevmvn/service/service/UserService.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 public class UserService {
 
     private final UserDao userDao = new UserDao();
+
     public Optional<UserDto> getUser(Long id) {
         return userDao.findById(id)
                 .map(it -> new UserDto());
