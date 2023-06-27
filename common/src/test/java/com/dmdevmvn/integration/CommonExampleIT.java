e31d79ea751ffec7ab1313b224519ba76f053588
DimaNever <dimanever86@gmail.com>
2023-06-27 14:11:06 +0600

    add IT tests

---
+++ common/src/test/java/com/dmdevmvn/integration/CommonExampleIT.java
@@ -0,0 +1,13 @@
+package com.dmdevmvn.integration;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CommonExampleIT {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
